485ce10946d7643bb66eeab6193604b6daa54fb9
Ian Listopad <ian@listopad.net>
2025-10-17 10:21:16 -0400

    add in providers.tf file and azrerm configuration

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "credlock-team-rg"
+    storage_account_name = "credlockterraform"
+    container_name = "terraformstate"
+    key = "terraformstatefile"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "e98e36a3-2842-4cc6-8286-2b44c456d59d"
+  features {}
+}
