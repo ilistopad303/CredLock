3442525952eb0ca1d454a65fd6df67266201a073
Ian Listopad <ian@listopad.net>
2025-10-21 18:00:04 -0400

    break out and add module call for linux VM's

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,17 +6,6 @@ resource "azurerm_network_security_group" "main-security-group" {
   location            = var.location
   name                = "credlock-network-security-group"
   resource_group_name = data.azurerm_resource_group.credlock-team-rg.name
-  security_rule {
-    name                       = "DenyAllInbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_virtual_network" "credlock-vm-network" {
@@ -25,9 +14,9 @@ resource "azurerm_virtual_network" "credlock-vm-network" {
   resource_group_name = data.azurerm_resource_group.credlock-team-rg.name
   address_space       = ["10.1.0.0/16"]
   subnet {
-    name           = "internal_subnet"
+    name             = "internal_subnet"
     address_prefixes = ["10.1.1.0/24"]
-    security_group = azurerm_network_security_group.main-security-group.id
+    security_group   = azurerm_network_security_group.main-security-group.id
   }
 }
 
@@ -37,11 +26,29 @@ data "azurerm_subnet" "subnet" {
   resource_group_name  = data.azurerm_resource_group.credlock-team-rg.name
 }
 
+module "windows_vm" {
+  source           = "./modules/vm"
+  windows_vm_names = ["credlock-vm1", "credlock-vm2"]
+  subnet_id        = data.azurerm_subnet.subnet.id
+  location         = var.location
+  source_image_reference = {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
 
-module "vm" {
-  source    = "./modules/vm"
-  vm_names  = ["credlock-vm1", "credlock-vm2"]
-  subnet_id = data.azurerm_subnet.subnet.id
-  location = var.location
+module "linux_vm" {
+  source         = "./modules/vm"
+  linux_vm_names = ["credlock-vm3", "credlock-vm4"]
+  subnet_id      = data.azurerm_subnet.subnet.id
+  location       = var.location
+  source_image_reference = {
+    publisher = "Canonical"
+    offer     = "ubuntu-22_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
 }
 
