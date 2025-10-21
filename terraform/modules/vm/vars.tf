1ed04bd48616269a1a9a1752c0c49804e546841d
Ian Listopad <ian@listopad.net>
2025-10-21 18:01:46 -0400

    add variables for things that are different between the two vm types

--- terraform/modules/vm/vars.tf
+++ terraform/modules/vm/vars.tf
@@ -1,9 +1,14 @@
-variable "vm_names" {
+variable "windows_vm_names" {
   description = "List of names for the virtual machines."
   type        = list(string)
   default     = []
 }
 
+variable "linux_vm_names" {
+  description = "List of names for the virtual machines."
+  type        = list(string)
+  default     = []
+}
 variable "subnet_id" {
   description = "The ID of the subnet where the VMs will be deployed."
   type        = string
@@ -13,4 +18,14 @@ variable "location" {
     description = "The Azure region where resources will be deployed."
     type        = string
     default     = "East US 2"
+}
+
+variable "source_image_reference" {
+    description = "The source image reference for the virtual machines."
+    type = object({
+        publisher = string
+        offer     = string
+        sku       = string
+        version   = string
+    })
 }
